b59086eedf595fca28151e5707dbc6c00c1c4681
6714_อุดมทรัพย์ <67319010014@bncc.ac.th>
2026-03-16 15:12:23 +0700

    Clean code setup

---
+++ src/config.php
@@ -0,0 +1,5 @@
+<?php
+// ไฟล์ src/config.php
+define('GOOGLE_CLIENT_ID', '528445645740-03tu4jaoovaj4ndmqsjt13474nc08elo.apps.googleusercontent.com');
+define('GOOGLE_CLIENT_SECRET', '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');
+?>
